ff2c354fa908d57e789f2bec0ef7bf7fb47f39d3
nihadmirzazada <13638@holbertonstudents.com>
2026-03-22 19:51:36 +0000

    task 12 fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,5 +9,8 @@ def fizzbuzz():
             print("Buzz", end="")
         else:
             print("{:d}".format(i), end="")
-        
-        print(" ", end="")
+
+        if i < 100:
+            print(" ", end="")
+        else:
+            print(" ", end="")
