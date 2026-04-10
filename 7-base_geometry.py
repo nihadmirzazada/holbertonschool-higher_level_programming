5d50b6d93eb695964760d0c2625101fc7e80c151
nihadmirzazada <13638@holbertonstudents.com>
2026-04-10 08:41:19 +0000

    fix tests and 7-base_geometry

---
+++ 7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Defines base geometry with integer validator."""
+
+    def area(self):
+        """Raise an Exception for unimplemented area."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate that value is a positive integer."""
+        if type(value) is not int or type(value) is bool:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
