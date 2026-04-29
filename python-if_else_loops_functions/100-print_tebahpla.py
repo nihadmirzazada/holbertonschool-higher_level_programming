3a52e3dbec1793db7f2b8f37734f2270396f158b
nihadmirzazada <13638@holbertonstudents.com>
2026-04-29 06:42:05 +0000

    MS F

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,8 @@
-results=""
-
-for idx, i in enumerate(range(122,96,-1)):
-   if idx%2==0:
-      results+= chr(i).lower()
-   else:
-    results += chr(i).upper()
-
-print(results, end = "")
+#!/usr/bin/python3
+results = ""
+for idx, i in enumerate(range(122, 96, -1)):
+    if idx % 2 == 0:
+        results += chr(i).lower()
+    else:
+        results += chr(i).upper()
+print("{}".format(results), end="")
